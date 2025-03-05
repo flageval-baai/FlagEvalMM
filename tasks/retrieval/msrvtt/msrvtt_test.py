61d06153910dbf122a65064cbe4f19c4417bdead
Zixi Yang <yangzixi@bupt.edu.cn>
2025-03-05 22:33:51 +0800

    fix comment

--- tasks/retrieval/msrvtt/msrvtt_test.py
+++ tasks/retrieval/msrvtt/msrvtt_test.py
@@ -1,10 +1,20 @@
+
+register_dataset = {"retrieval_msrvtt_dataset.py": "retrievalMsrvttDataset"}
+register_evaluator = {"video_retrieval_evaluator.py": "videoRetrievalEvaluator"}
+
 config = dict(
-    dataset_path="nlphuji/msrvtt_video_text_retrieval",
+    dataset_path="BAAI/retrieval/msrvtt",
     split="test",
-    processed_dataset_path="retrieval/msrvtt",
+    processed_dataset_path="retrival/msrvtt",
     processor="../msrvtt/msrvtt_process.py",
 )
 
-dataset = dict(type="MSRVTT_single_sentence_dataLoader", config=config, name="msrvtt")
+dataset = dict(
+    type="retrivalMsrvttDataset",
+    config=config,
+    name="msrvtt",
+)
 
-evaluator = dict(type="VideoRetrievalEvaluator")
+evaluator = dict(
+    type="CmmuEvaluator",
+)
