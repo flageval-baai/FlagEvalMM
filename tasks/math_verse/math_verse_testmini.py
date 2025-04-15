ad7c8a9f693cb9adf76211ba8f634fd4cd5c852c
zhizhou57 <ylxg57ys@163.com>
2025-04-15 10:58:34 +0800

    restore old mathverse

--- tasks/math_verse/math_verse_testmini.py
+++ tasks/math_verse/math_verse_testmini.py
@@ -8,23 +8,29 @@ config = dict(
     processor="process.py",
 )
 
+
+def pre_prompt(question_type: str, **kwargs) -> str:
+    if question_type == "multi-choice":
+        prompt = "According to the question shown in the image, please directly answer the question and provide the correct option letter, e.g., A, B, C, D.\nQuestion: "
+    else:
+        prompt = "According to the question shown in the image, please directly answer the question and provide the final value, e.g., 1, 2.5, 300.\nQuestion: "
+    return prompt
+
+
 dataset = dict(
     type="VqaBaseDataset",
     config=config,
-    prompt_template=dict(
-        type="PromptTemplate",
-        pre_prompt="",
-        post_prompt="Please solve the problem and put your answer in one '\\boxed{}'",
-    ),
+    prompt_template=dict(type="PromptTemplate", pre_prompt=pre_prompt, post_prompt=""),
     name="math_verse_testmini",
 )
 
-
 evaluator = dict(
-    type="ExtractEvaluator",
-    eval_model_name="gpt-4o-mini",
+    type="BaseEvaluator",
+    eval_func="evaluate.py",
     use_llm_evaluator=True,
     use_cache=True,
     base_url=os.getenv("FLAGEVAL_BASE_URL"),
     api_key=os.getenv("FLAGEVAL_API_KEY"),
+    model_name="gpt-4o-mini",
+    chat_name="math_verse_eval",
 )
