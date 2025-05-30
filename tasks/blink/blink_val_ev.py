6ce5fddf87f41f9a18ee1da5ae73da67064d3042
philokey <philokeys@gmail.com>
2025-05-30 19:13:18 +0800

    add embodied verse subset for blink

--- tasks/blink/blink_val_ev.py
+++ tasks/blink/blink_val_ev.py
@@ -1,3 +1,4 @@
+# Subset for EmbodiedVerse
 task_name = "vqa"
 
 config = dict(
@@ -6,31 +7,24 @@ config = dict(
     processed_dataset_path="BLINK",
     processor="process.py",
     dataset_names=[
-        "Art_Style",
         "Counting",
-        "Forensic_Detection",
-        "Functional_Correspondence",
-        "IQ_Test",
-        "Jigsaw",
-        "Multi-view_Reasoning",
-        "Object_Localization",
         "Relative_Depth",
-        "Relative_Reflectance",
-        "Semantic_Correspondence",
         "Spatial_Relation",
+        "Multi-view_Reasoning",
         "Visual_Correspondence",
-        "Visual_Similarity",
     ],
+    anno_file="data_ev.json",
 )
 
 dataset = dict(
     type="VqaBaseDataset",
+    anno_file="data_ev.json",
     config=config,
     prompt_template=dict(
         type="PromptTemplate",
         post_prompt="The last line of your response should be of the following format: 'Answer: $LETTER' (without quotes) where LETTER is one of options.",
     ),
-    name="blink_val",
+    name="blink_val_ev",
 )
 
 evaluator = dict(type="BaseEvaluator", detailed_keys=["sub_task"])
