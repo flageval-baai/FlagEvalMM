59af61265d7129276ee1e4c4f9dbe12661c8632b
xueweihan <595666367@qq.com>
2025-06-10 23:29:16 +0800

    update mvbench task

--- tasks/mvbench/mvbench_test.py
+++ tasks/mvbench/mvbench_test.py
@@ -1,18 +1,19 @@
-register_evaluator = {"mvbench_dataset_evaluator.py": "MVBenchbenchDatasetEvaluator"}
-
 config = dict(
     dataset_path="OpenGVLab/MVBench",
     split="test",
     processed_dataset_path="MVBench",
-    processor="mvbench_process.py",
+    processor="process.py",
 )
 
 
 dataset = dict(
     type="VideoDataset",
+    prompt_template=dict(
+        type="PromptTemplate",
+        pre_prompt="Carefully watch the video and pay attention to the cause and sequence of events, the detail and movement of objects, and the action and pose of persons. Based on your observations, select the best option that accurately addresses the question.",
+    ),
     config=config,
-    anno_file="data.json",
     name="mvbench",
 )
 
-evaluator = dict(type="MVBenchDatasetEvaluator")
+evaluator = dict(type="BaseEvaluator", detailed_keys=["sub_task"])
