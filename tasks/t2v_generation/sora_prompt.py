3c587a2dfdba2e66c083398dbec254246de27e93
Yesheng Liu <liuyesheng24@mails.ucas.ac.cn>
2025-07-25 18:36:16 +0800

    fix path and model name (#79)

--- tasks/t2v_generation/sora_prompt.py
+++ tasks/t2v_generation/sora_prompt.py
@@ -2,7 +2,8 @@ task_name = "t2v_generation"
 
 dataset = dict(
     type="Text2VideoBaseDataset",
-    data_root="/share/project/mmdataset/sora_prompts_reformat.json",
+    data_root="/share/project/mmdataset",
+    anno_file="sora_prompts_reformat.json",
     name="sora_prompt",
 )
 
