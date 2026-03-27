138bbe8501776569525eccfb8500d212afd9f82a
philokey <philokeys@gmail.com>
2026-03-27 18:35:26 +0800

    fix text vqa

--- tasks/text_vqa/text_vqa_val.py
+++ tasks/text_vqa/text_vqa_val.py
@@ -1,5 +1,5 @@
 config = dict(
-    dataset_path="facebook/textvqa",
+    dataset_path="lmms-lab/textvqa",
     split="validation",
     processed_dataset_path="textvqa",
     processor="process.py",
